857914f012391db1c5ac066cb72c51f4cc6b599c
Praneesh <parshipraneesh8@gmail.com>
2024-04-06 09:16:00 +0530

    events repair

--- FRONTEND/src/components/Events/Events.jsx
+++ FRONTEND/src/components/Events/Events.jsx
@@ -1,32 +1,29 @@
 import "./Events.css";
 import  events from "./events-info.js";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {useState} from "react";
 export default function Events() {
   const navigate = useNavigate();
-  const [clicked, setClicked] = useState(false);
-  function handleClick(){
-    setClicked(true);
+  const [i, setI]= useState(0);
+  const [year, setYear] = useState(2023);
+  function handleClick(e){
+    e.target.innerHTML === "2023" &&  (setI(0) || setYear(2023));
+    e.target.innerHTML === "2022" && ( setI(1) || setYear(2022));
   }
   return (
     <div className="events">
-      <h1>Events</h1>
+      <h1>Events of the year {year}</h1>
+      <div className="events-buttons">
+        <button onClick={handleClick}>2023</button>
+        <button onClick={handleClick}>2022</button>
+      </div>
       <div className="event-box-cont">
-        {events[0].map((event, index) => (
+        {events[i].map((event, index) => (
           <div key={index} className="event-box" onClick={()=>navigate(event.link)} >
             <h2>{event.name}</h2>
             <p>{event.description}</p>
           </div>
         ))}
-      </div>
-      <div className="past-events">
-        <h2>Past Events</h2>
-        <div className="event-box-cont">
-          {/* should show buttons of events2022,etc */}
-          <button onClick={handleClick(true)}>  
-            2022
-          </button>
-        </div>
       </div>
     </div>
   );
