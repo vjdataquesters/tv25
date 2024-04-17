853d0c841b290815dce7f447a98021c386512cab
karthikeyaspace <veruturikarthikeya@gmail.com>
2024-04-17 11:36:01 +0530

    added team images, scroll animation using framer motion, home screen mods
    signed-off-by: karthikeyaspace<veruturikarthikeya@gmail.com>

--- FRONTEND/src/components/Home/Landing.jsx
+++ FRONTEND/src/components/Home/Landing.jsx
@@ -9,20 +9,14 @@ export default function Landing() {
 
         <div className="h1-div">
             {/* <Particle className="particles"/> */}
-
-
-
             <div className="landing-info">
                 <div className="landing-logo">
-                    <span>
                         <img src="dq_logo.png" alt="" />
-                    </span>
                 </div>
 
                 <div className="landing-text">
                     <h1>VJ Data Questers</h1>
                     <h3>VNR VJIET, hyderabad</h3>
-                    <br />
                     <p>LEARN IMPLEMENT COLLABORATE</p>
                 </div>
             </div>
