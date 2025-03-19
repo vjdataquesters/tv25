e3e46ed11da807328a23308dd5a520a5f26ecb6b
Jagadeeswar_J <115331104+Jagadeeswar072@users.noreply.github.com>
2025-03-19 21:39:40 +0530

    loading animations

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ function App() {
   return (
     <Router>
       <Analytics />
-      {/* <Loading load={load} /> */}
+      <Loading load={load} />
       <Header />
       <ScrollToTop />
       <div className="flex flex-col min-h-screen bg-blue-50/70">
