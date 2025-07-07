4ff3d92ffbaa35e508a84b78ab682e72729e6012
PPraneesh <parshipraneesh8@gmail.com>
2025-07-07 14:27:23 +0530

    remove unnecessary things in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ["3d76-43-228-92-217.ngrok-free.app"]
-  }
 })
